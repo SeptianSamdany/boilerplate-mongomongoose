0dad9612def8e7ad6b56e0ba805c47c7f7a910ea
SeptianSamdany <227007072@student.unsil.ac.id>
2024-10-12 22:09:50 +0700

    Initial commit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,16 @@
 require('dotenv').config();
 
+/** # MONGOOSE SETUP #
+/*  ================== */
+
+/** 1) Install & Set up mongoose */
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
 let Person;
 
